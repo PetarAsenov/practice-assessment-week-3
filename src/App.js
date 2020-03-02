b228546af57e61d1f88266e2e9d4e727d204160b
PetarAsenov <p.a.asenov@gmail.com>
2020-03-02 11:24:38 +0100

    make a navbar and links to all pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route, Link, NavLink} from "react-router-dom";
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
 import HomePage from './components/HomePage'
 import DoctorView from './components/DoctorView'
 import OnDuty from './components/OnDuty'
@@ -15,6 +16,12 @@ function App() {
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
         crossorigin="anonymous"
       />
+      <Navbar>
+        <Link to="/">Home page</Link>
+        <Link to="/onduty">Who is on duty</Link>
+        <Link to="/signup">Sign up</Link>
+        <Link to="/doctorview">Doctor's view</Link>
+      </Navbar>
       <Switch>
         <Route path="/onduty" component={OnDuty} />
         <Route path="/doctorview" component={DoctorView} />
