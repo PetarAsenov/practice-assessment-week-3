0fad3dc47a4f489a7d184b41ac32f6dbbcc635c0
PetarAsenov <p.a.asenov@gmail.com>
2020-03-02 11:10:54 +0100

    create the frame and path of the first four components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, Link, NavLink} from "react-router-dom";
 import './App.css';
+import HomePage from './components/HomePage'
+import DoctorView from './components/DoctorView'
+import OnDuty from './components/OnDuty'
+import NewPatientForm from './components/NewPatienForm'
 
 function App() {
   return (
     <div className="App">
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+        crossorigin="anonymous"
+      />
+      <Switch>
+        <Route path="/onduty" component={OnDuty} />
+        <Route path="/doctorview" component={DoctorView} />
+        <Route path="/signup" component={NewPatientForm} />
+        <Route path="/" component={HomePage} />
+      </Switch>
 
     </div>
   );
